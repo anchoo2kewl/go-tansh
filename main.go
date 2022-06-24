2d06ea8584d34afe53a429d4239c8d53512b7d00
Anshuman Biswas <anshuman@biswas.me>
2022-06-24 14:55:46 -0400

    Adding chi as the router library

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os/exec"
 	"runtime"
 	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func getRuntime(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +26,10 @@ func getRuntime(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:anshuman@biswas.me\">anshuman@biswas.me</a>.")
+	_, err := fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:anshuman@biswas.me\">anshuman@biswas.me</a>.")
+	if err != nil {
+		return 
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,21 +42,40 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	getRuntime(w, r)
 }
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:anshuman@biswas.me">anshuman@biswas.me</a>
+  </li>
+</ul>
+`)
 }
 
 type Router struct {}
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe("127.0.0.1:3000", r)
+	if err != nil {
+		return
+	}
 }
